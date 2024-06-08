7d86e274f724ef724407db51ea0dd7fe6d3a75f0
richardesp <espantaleon2001@gmail.com>
2024-06-08 22:37:34 +0200

    refactor: Delete NFT view

--- src/routes.js
+++ src/routes.js
@@ -28,21 +28,6 @@ const routes = [
     component: MainDashboard,
   },
   {
-    name: "NFT Marketplace",
-    layout: "/admin",
-    path: "/nft-marketplace",
-    icon: (
-      <Icon
-        as={MdOutlineShoppingCart}
-        width='20px'
-        height='20px'
-        color='inherit'
-      />
-    ),
-    component: NFTMarketplace,
-    secondary: true,
-  },
-  {
     name: "Data Tables",
     layout: "/admin",
     icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
